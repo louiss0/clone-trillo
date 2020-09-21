b1095c54970c4449df0a5330edddd828b3796533
Shelton <louisshelton0@gmail.com>
2020-09-20 20:31:25 -0400

    finished project

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as DOMSelectors from "./ts/utils/selector";
-
-// ! Im selecting by id
-const app = DOMSelectors.selectElementByClassOrId("#app")
-
-if (app) {
-    app.textContent = "Hello World"
-}
-
